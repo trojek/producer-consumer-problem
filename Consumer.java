0f7f266c75c8f542840b5d38d1130d187bc5a752
Tomasz Rojek <tomek@rojek.cc>
2013-11-26 21:34:25 +0100

    print avanger from consumed integers

--- Consumer.java
+++ Consumer.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+
 public class Consumer extends Thread {
 
 	Buffer buf;
 	int sleepTime, consumerNumber;
 	boolean stop = false;
+	
+	ArrayList<Integer> c1 = new ArrayList<Integer>();
+	ArrayList<Integer> c2 = new ArrayList<Integer>();
 
 	public Consumer(int st, int cn) {
 		sleepTime = st;
@@ -15,11 +20,11 @@ public class Consumer extends Thread {
 				int result = buf.get();
 				if (result >= 0) {
 					if ((result <= 10) && consumerNumber == 1) {
-						System.out.println("Consumer " + consumerNumber
-								+ " get:" + result);
+						System.out.println("Consumer " + consumerNumber + " get:" + result);
+						c1.add(result);
 					} else if ((result >= 10) && consumerNumber == 2) {
-						System.out.println("Consumer " + consumerNumber
-								+ " get:" + result);
+						System.out.println("Consumer " + consumerNumber	+ " get:" + result);
+						c2.add(result);
 					}
 					sleep(sleepTime);
 				}
@@ -27,5 +32,16 @@ public class Consumer extends Thread {
 				System.err.println(e);
 			}
 		}
+		
+		if (consumerNumber == 1) System.out.println("C2: " + sumOfArrayList(c1)/c1.size());
+		if (consumerNumber == 2) System.out.println("C2: " + sumOfArrayList(c2)/c2.size());
+		
+	}
+
+	double sumOfArrayList(ArrayList<Integer> al) {
+		double sum = 0;
+		for(Integer d : al)
+		    sum += d;
+		return sum;
 	}
 }
